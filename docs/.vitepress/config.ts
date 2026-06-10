8c364fa5ee71dc61f7159afde0740c9cf8bcecd5
Taullarkura <Tuallakura@qq.com>
2026-06-10 21:35:24 +0800

    Initization

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base : "/",
+  title: "废墟图书馆mod教程",
+  description: "测试用喵？",
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文章', link: '/posts' }
+    ],
+    // 侧边栏可配，博客模式下一般用 frontmatter 自动生成
+  }
+})
